079e746db2d289c164df845f13bb7441f06700a0
ProjectivePlane <derek.bell.ie@gmail.com>
2016-11-20 18:38:32 +0000

    Add toInteger methods to convert Roman numerals to integers.

--- build.sbt
+++ build.sbt
@@ -2,8 +2,8 @@ name := "RomanNumerals"
 
 version := "1.0"
 
-scalaVersion := "2.11.7"
+scalaVersion := "2.12.0"
 
-libraryDependencies += "org.scalactic" %% "scalactic" % "2.2.6"
-libraryDependencies += "org.scalatest" %% "scalatest" % "2.2.6"
+libraryDependencies += "org.scalactic" %% "scalactic" % "3.0.0"
+libraryDependencies += "org.scalatest" %% "scalatest" % "3.0.0"
 
--- src/main/scala/romannumerals/RomanNumeral.scala
+++ src/main/scala/romannumerals/RomanNumeral.scala
@@ -4,10 +4,10 @@ import scala.collection.immutable.Map
 import scala.collection.immutable.List
 import scala.collection.mutable.ListBuffer
 
-
 object RomanNumeral{
 val romanDigits=collection.immutable.Map(1-> "I",4->"IV",5->"V",9->"IX",10->"X",40->"XL",50->"L",90->"XC",100->"C",400->"CD",500->"D",900->"CM",1000->"M")
 val romanDigitValues=collection.immutable.List(1000,900,500,400,100,90,50,40,10,9,5,4,1)
+val romanDigitValueMap=collection.immutable.Map('I'->1,'V'->5,'X'->10,'L'->50,'C'->100,'D'->500,'M'->1000)
 
 	def toRomanNumeral(n:Integer)={
            romanDigits(n)
@@ -31,4 +31,11 @@ val romanDigitValues=collection.immutable.List(1000,900,500,400,100,90,50,40,10,
 	   }
 	   result.toList
 	}
+
+        def toInteger(s:String)={
+           var digitValues=s.toUpperCase.map(c => romanDigitValueMap(c))
+	   var pair= digitValues.foldLeft((0,0)){
+	    case((sum,last),curr) => (sum+curr+(if (last<curr) -2*last else 0),curr)}
+	   pair._1
+        }
 }
